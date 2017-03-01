a22704269891469a0769d49de0774bc6eb600c1d
Farrell Meng <mengfanqi0828@163.com>
2017-03-01 21:30:52 +0800

    删除一些不必要的头文件。

--- 图像拼接01/01.cpp
+++ 图像拼接01/01.cpp
@@ -1,13 +1,5 @@
-
-#include <stdio.h>  
-#include <iostream>  
-#include <cv.h>  
-#include "opencv2/core/core.hpp"  
+#include <iostream>   
 #include <opencv2/opencv.hpp>    
-#include "opencv2/features2d/features2d.hpp"  
-#include "opencv2/highgui/highgui.hpp"  
-#include "opencv2/calib3d/calib3d.hpp"  
-#include <opencv2/imgproc/imgproc.hpp>  
 #include "opencv2/xfeatures2d.hpp"
 
 using namespace cv;
